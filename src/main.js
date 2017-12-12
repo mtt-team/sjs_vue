6757d03a7a7c895be92ebffd31f66c51cd17596b
jgzhang <1103610594@qq.com>
2017-12-12 10:10:40 +0800

    main.js添加 import { currency } from './currency'
    Vue.filter('currency', currency)
    用来解析货币

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import store from './store'
+import { currency } from './currency'
+
+Vue.filter('currency', currency)
 
 Vue.use(ElementUI)
 Vue.use(iView);
